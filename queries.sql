ce1e72cd8442f22690fdbe836cf2aa9f92a32983
Jessica Bauman <jbauman815@gmail.com>
2022-11-27 23:56:07 -0500

    adding the assignment

--- queries.sql
+++ queries.sql
@@ -100,3 +100,124 @@ WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
 AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
 
 SELECT * FROM retirement_info;
+
+SELECT * FROM salaries
+ORDER BY to_date DESC;
+
+SELECT emp_no, first_name, last_name
+INTO retirement_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+SELECT e.emp_no,
+    e.first_name,
+e.last_name,
+    e.gender,
+    s.salary,
+    de.to_date
+
+--INTO emp_info
+FROM employees as e
+INNER JOIN salaries as s
+ON (e.emp_no = s.emp_no)
+
+INNER JOIN dept_emp as de
+ON (e.emp_no = de.emp_no)
+
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+     AND (e.hire_date BETWEEN '1985-01-01' AND '1988-12-31')
+  AND (de.to_date = '9999-01-01');
+
+
+
+SELECT  dm.dept_no,
+        d.dept_name,
+        dm.emp_no,
+        ce.last_name,
+        ce.first_name,
+        dm.from_date,
+        dm.to_date
+--INTO manager_info
+FROM dept_manager AS dm
+    INNER JOIN departments AS d
+        ON (dm.dept_no = d.dept_no)
+    INNER JOIN current_emp AS ce
+        ON (dm.emp_no = ce.emp_no);
+
+
+
+
+SELECT ce.emp_no,
+ce.first_name,
+ce.last_name,
+d.dept_name
+-- INTO dept_info
+
+FROM current_emp as ce
+INNER JOIN dept_emp AS de
+ON (ce.emp_no = de.emp_no)
+INNER JOIN departments AS d
+ON (de.dept_no = d.dept_no);
+
+
+
+SELECT employees.emp_no, 
+first_name, 
+last_name, 
+dept_name
+INTO sales_retirement_info
+FROM employees 
+INNER JOIN dept_emp 
+ON (employees.emp_no = dept_emp.emp_no)
+INNER JOIN departments
+ON (dept_emp.dept_no = departments.dept_no)
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31')
+AND departments.dept_name = 'Sales';
+
+
+
+
+SELECT current_emp.emp_no, 
+first_name, 
+last_name, 
+dept_name
+INTO current_emp_sales_info
+FROM current_emp 
+INNER JOIN dept_emp 
+ON (current_emp.emp_no = dept_emp.emp_no)
+INNER JOIN departments
+ON (dept_emp.dept_no = departments.dept_no)
+WHERE departments.dept_name = 'Sales';
+SELECT * FROM current_emp_sales_info
+
+
+
+SELECT current_emp.emp_no, 
+first_name, 
+last_name, 
+dept_name
+INTO current_emp_sales_development_info
+FROM current_emp 
+INNER JOIN dept_emp 
+ON (current_emp.emp_no = dept_emp.emp_no)
+INNER JOIN departments
+ON (dept_emp.dept_no = departments.dept_no)
+WHERE departments.dept_name = 'Sales' OR departments.dept_name = 'Development';
+SELECT * FROM current_emp_sales_development_info
+
+
+
+SELECT current_emp.emp_no, 
+first_name, 
+last_name, 
+dept_name
+INTO current_emp_sales_development_info
+FROM current_emp 
+INNER JOIN dept_emp 
+ON (current_emp.emp_no = dept_emp.emp_no)
+INNER JOIN departments
+ON (dept_emp.dept_no = departments.dept_no)
+WHERE departments.dept_name IN ('Sales', 'Development');
+SELECT * FROM current_emp_sales_development_info
